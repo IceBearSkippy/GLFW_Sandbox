d34d58b1d3eb3dfcc7f59a5e7e84e775e6f329ba
Sanjay Sagar <icebearskippy@gmail.com>
2020-04-18 14:11:28 -0700

    Create color interpolation shader with vbo with animation for cube

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cmath>
 
 // GLEW
 #define GLEW_STATIC
@@ -27,45 +28,99 @@
 using namespace std;
 
 #define numVAOs 1
+#define numVBOs 2
 
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
 
-float x = 0.0f;
-float inc = 0.01f;
-mat4 rotateMat;
+//allocate variables used in display function
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
+
+void setupVertices(void) {
+    float vertexPositions[108] = {
+        -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 1.0f, -1.0f,
+
+        1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, -1.0f,
+        1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, 1.0f,
+        -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f,
+        1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f,
+        -1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, 1.0f,
+        1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, -1.0f
+    };
+    glGenVertexArrays(1, vao);
+    glBindVertexArray(vao[0]);
+    glGenBuffers(numVBOs, vbo); // two vbos are created, but only is used!
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
+}
 
 void init(GLFWwindow* window) {
     renderingProgram = Utilities::createShaderProgram("./res/shaders/practice.vert", "./res/shaders/practice.frag");
-    glGenVertexArrays(numVAOs, vao);
-    glBindVertexArray(vao[0]);
-    rotateMat = Utilities::buildRotateX(10);
-    cout << glm::to_string(rotateMat) << endl;
+    cameraX = 0.0f; cameraY = 0.0f; cameraZ = 8.0f;
+    cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+    setupVertices();
 }
 
 void display(GLFWwindow* window, double currentTime) {
     glClear(GL_DEPTH_BUFFER_BIT);
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT); // this references the color buffer that contains the pixels as they are rendered
+    glClear(GL_COLOR_BUFFER_BIT);
 
     glUseProgram(renderingProgram);
 
-    x += inc;
-    if (x > 30.0f) {
-        inc = -1.0f;
-    }
-    if (x < -30.0f) {
-        inc = 1.0f;
-    }
+    // get the uniform variables for MV and projection matrices
+    mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+    projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-    //GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
-    //glProgramUniform1f(renderingProgram, offsetLoc, 0); // get the variable and setting it to x
+    //build perspective matrix
+    glfwGetFramebufferSize(window, &width, &height);
+    aspect = (float)width / (float)height;
+    pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0672 radians = 60 degrees
 
-    rotateMat = Utilities::buildRotateY(x);
-    GLuint tranformationLoc = glGetUniformLocation(renderingProgram, "transformation");
-    glProgramUniformMatrix4fv(renderingProgram, tranformationLoc, 1, GL_FALSE, glm::value_ptr(rotateMat));
-    //glPointSize(30.0f);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //build view, model and model-view matrix
+    vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+
+    // use current time to compute different tranlations x, y and z
+    //tMat = glm::translate(glm::mat4(1.0f),
+    //    glm::vec3(sin(0.35f * currentTime) * 2.0f, cos(0.52f * currentTime) * 2.0f, sin(0.7f * currentTime) * 2.0f));
+    tMat = Utilities::buildTranslate(sin(0.35f * currentTime) * 2.0f, cos(0.52f * currentTime) * 2.0f, sin(0.7f * currentTime) * 2.0f);
+
+    //rMat = glm::rotate(glm::mat4(1.0f), 1.75f * (float)currentTime, glm::vec3(0.0f, 1.0f, 0.0f));
+    //rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
+    //rMat = glm::rotate(rMat, 1.75f * (float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+
+    rMat = Utilities::buildRotateX(1.75 * (float)currentTime) * Utilities::buildRotateY(1.75 * (float)currentTime) * Utilities::buildRotateZ(1.75 * (float)currentTime);
+
+    // when vertex is multiplayed by this matrix, it works right to left
+    // eg rotation will be applied first and then translation
+    mMat = tMat * rMat;
+    mvMat = vMat * mMat;
+
+    // copy perspective/MV matrices to uniform matrix variables
+    glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+    glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+    //associate VBO with corresponding vertex attribute in vertex shader
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    //Adjust OpenGL settings / draw
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
 }
 int main(void) {
@@ -76,7 +131,7 @@ int main(void) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 4 - program1", NULL, NULL);
     glfwMakeContextCurrent(window);
 
     if (glewInit() != GLEW_OK) {
